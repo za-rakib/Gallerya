147c04e68177a4f0eb2773f44619142016e11590
Rakib <rakib@sayburgh.com>
2024-09-15 11:46:29 +0600

    redux test

---
+++ src/screens/Redux.jsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const Redux = () => {
+  return (
+    <View style={{height: '100$%', width: '100%', backgroundColor: '#000'}}>
+      <Text>Redux</Text>
+    </View>
+  );
+};
+
+export default Redux;
